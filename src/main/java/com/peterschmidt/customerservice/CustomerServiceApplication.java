6e9e7af14a9fe626a5ffad0e30f7628e380b6cd1
p-schm1dt <p.schmidt2001@gmail.com>
2023-08-27 18:17:03 +0200

    initial commit

---
+++ src/main/java/com/peterschmidt/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,25 @@
+package com.peterschmidt.customerservice;
+
+import com.peterschmidt.customerservice.model.Customer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/customer")
+
+public class CustomerServiceApplication {
+// test
+	@RequestMapping("/{customerId}")
+	public Customer getCustomer(@PathVariable("customerId") int id){
+		return new Customer(id,"Max", "Mustermann", "max@mustermann.com");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peterschmidt/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peterschmidt.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
